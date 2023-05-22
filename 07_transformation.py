bef4fbac61d1d19eb15dfad2f16175be0dd1cb0a
navinocrala <navinocrala@gmail.com>
2023-05-21 22:33:41 -0400

    Agregado nuevo archivo

---
+++ 07_transformation.py
@@ -0,0 +1,20 @@
+name = "Iván"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Iván" + "Alarcón")
+print(10 + 20)
+print("Iván" + "30")
+
+age = 31
+print ("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input("Escribe tu edad")
+print(type(age))
+age = int(age)
+age += 10
+print(f"Tu edad en 10 años será {age}")
