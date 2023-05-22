14242e67702cb18fa08947b6a55584fff58bd9d2
navinocrala <navinocrala@gmail.com>
2023-05-21 22:48:06 -0400

    Aguregado nuevos archivos

---
+++ 08_arith.py
@@ -0,0 +1,13 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 3)
+print(10 % 2)
+print(10 % 3) #obtener resíduo
+print(10 // 3) #obtener entero
+print(2 ** 3) #exponente
+print(2 ** 3 + 3 - 7 / 1 // 4)
+#print(10/0)
+
+print("Hola" + "mundo")
+print("Hola" * 3)
